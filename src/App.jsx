840e28b4cb2ea744c9b2a07c05f9cd469f0f5265
Dennis <suwatchai.kaew@gmail.com>
2023-12-25 23:23:52 +0700

    feat: add finished ex2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,48 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
-  return <div className="App">{/* start coding here */}</div>;
+  const [search, setSearch] = useState("");
+  const [books, setBooks] = useState();
+  const [error, setError] = useState("");
+  useEffect(() => {
+    !search ? "" : getBook();
+  }, [search]);
+
+  async function getBook() {
+    try {
+      const data = await axios.get(
+        `https://www.googleapis.com/books/v1/volumes?q=${search}`
+      );
+      setBooks(data.data.items);
+      console.log(data);
+    } catch (error) {
+      setError(error.message);
+      console.log(error);
+    }
+  }
+
+  return (
+    <div className="App">
+      <h1>Find a Book</h1>
+      <input
+        minLength={2}
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <ul>
+        {!books ? (
+          <p>{error}</p>
+        ) : (
+          books.map((book, index) => {
+            return <li key={index}>{book.volumeInfo.title}</li>;
+          })
+        )}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
